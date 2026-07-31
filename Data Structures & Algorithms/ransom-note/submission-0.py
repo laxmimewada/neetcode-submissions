c70e98f10a37aa25c09ad9ad5d7bf9601858c128
Laxmi Mewada <laxmimewada47@gmail.com>
2026-07-31 13:15:52 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dictionary = {}
+
+        for char in magazine:
+            if char in dictionary:
+                dictionary[char] += 1
+            else:
+                dictionary[char] = 1
+        
+        for char in ransomNote:
+            if char in dictionary and dictionary[char] > 0:
+                dictionary[char] -= 1
+            else:
+                return False
+
+        return True
+        
